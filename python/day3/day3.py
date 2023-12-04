1c8daa21f71cf16fc74385199ef8fa80bdcb79b9
Liam Mclaughlin <liammcl33@gmail.com>
2023-12-04 18:12:14 +0900

    day 4

--- python/day3/day3.py
+++ python/day3/day3.py
@@ -14,10 +14,12 @@ rows = len(data)
 cols = len(data[0])
 
 answers = []
+gears = collections.defaultdict(int)
 
 for i in range(rows):
     num = ''
     possible = False
+    gear_loc = None
     for j in range(cols):
         char = data[i][j]
         if char.isnumeric():
@@ -27,13 +29,22 @@ for i in range(rows):
                 if 0 <= new_row < rows and 0 <= new_col < cols:
                     if not data[new_row][new_col].isnumeric() and data[new_row][new_col] != '.':
                         possible = True
-
+                        if data[new_row][new_col] == '*':
+                          gear_loc = (new_row, new_col)
+                        
         if j == cols - 1 or not char.isnumeric():
             if possible:
-                answers.append(num) 
+                answers.append(num)
+                if gear_loc:
+                    if gears[gear_loc]:
+                        gears[gear_loc] = [gears[gear_loc], int(num)]
+                    else:
+                        gears[gear_loc] = int(num)
             num = ''
             possible = False
-    
+            gear_loc = None
+
+print(sum([prod(part) for part in gears.values() if type(part) == list]))
 print(sum([int(a) for a in answers]))
 
 
---
+++ python/day4/day4.py
@@ -0,0 +1,53 @@
+from math import prod
+import collections
+
+def read_data_from_file(file_path):
+    with open(file_path, 'r') as file:
+        data = file.readlines()
+    return [line.strip() for line in data]
+
+file_path = '2023/python/day4/data.txt'
+data = read_data_from_file(file_path)
+
+
+points = 0
+
+for line in data:
+    score = 0
+    win_nums = [int(x) for x in line.split(':')[1].split('|')[0].strip().split(' ') if x.isnumeric()]
+    guesses = [int(x) for x in line.split(':')[1].split('|')[1].strip().split(' ') if x.isnumeric()]
+    matches = 0
+    for guess in guesses:
+        if guess in win_nums:
+            matches += 1
+    if matches > 0:
+        score =  1 * 2 ** (matches - 1)
+    points += score
+
+#print(points)
+
+##part2 
+
+counts = [1] * len(data)
+cards = collections.defaultdict(int)
+
+for i, line in enumerate(data):
+    matches = 0
+    win_nums = [int(x) for x in line.split(':')[1].split('|')[0].strip().split(' ') if x.isnumeric()]
+    guesses = [int(x) for x in line.split(':')[1].split('|')[1].strip().split(' ') if x.isnumeric()]
+    for guess in guesses:
+        if guess in win_nums:
+            matches += 1
+    cards[i] = matches
+
+for i in range(len(data)):
+    for j in range(i + 1, i + cards[i] + 1):
+        counts[j] += counts[i]
+        print(f'Your {counts[i]} copies of card {i} have {cards[i]} matching numbers, so you win {counts[i]} copies of card {j}')
+
+print(sum(counts))
+
+
+    
+
+
