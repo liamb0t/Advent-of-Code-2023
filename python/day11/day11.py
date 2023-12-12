4f529e3fb8ff9e525d2ec15e917aa7fb7006a73c
Liam Mclaughlin <liammcl33@gmail.com>
2023-12-12 11:32:28 +0900

    refactoring

--- python/day11/day11.py
+++ python/day11/day11.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 def read_data_from_file(file_path):
     with open(file_path, 'r') as file:
         data = file.readlines()
@@ -5,12 +7,12 @@ def read_data_from_file(file_path):
 
 file_path = '2023/python/day11/data.txt'
 universe = [[x for x in line] for line in read_data_from_file(file_path)]
-galaxies = {}
+galaxies = []
 rows = len(universe)
 cols = len(universe[0])
 empty_rows = []
 empty_cols = []
-galaxy_num = 1
+expansion = 1000000 - 1
 
 for i in range(rows):
     row = universe[i]
@@ -20,33 +22,29 @@ for i in range(rows):
     for j in range(cols): 
         col.append(universe[j][i])
         if universe[i][j] == '#':
-            galaxies[galaxy_num] = (i, j)
-            galaxy_num += 1
+            galaxies.append((i, j))
     if all(x=='.' for x in col):
         empty_cols.append(i)
 
 def move_galaxy(node):
     row, col = node[0], node[1]
-    for empty_row in empty_rows:
+    for empty_row in empty_rows[::-1]:
         if node[0] > empty_row:
-            row = node[0] + ((empty_rows.index(empty_row) + 1) * 10)
-    for empty_col in empty_cols:
+            row = node[0] + ((empty_rows.index(empty_row) + 1) * expansion)
+            break
+    for empty_col in empty_cols[::-1]:
         if node[1] > empty_col:
-            col = node[1] + ((empty_cols.index(empty_col) + 1) * 10)
+            col = node[1] + ((empty_cols.index(empty_col) + 1) * expansion)
+            break
     return (row, col)
 
-shifted_galaxies = {}
-for i, coords in enumerate(list(map(move_galaxy, galaxies.values())), 1):
-    shifted_galaxies[i] = coords
+shifted_galaxies = list(map(move_galaxy, galaxies))
+num_galaxies = len(galaxies)
+total = 0
 
-num_galaxies = len(galaxies.keys())
-sum = 0
+for p1, p2 in combinations(shifted_galaxies, 2):
+    x1, y1 = p1
+    x2, y2 = p2
+    total += abs(x1 - x2) + abs(y1 - y2)
 
-for i in range(1, num_galaxies + 1):
-    x1, y1 = shifted_galaxies[i]
-    for j in range(i, num_galaxies + 1):
-        x2, y2 = shifted_galaxies[j]
-        if i != j:
-            sum += abs(x1 - x2) + abs(y1 - y2)
-            
-print(sum)
+print(total)
