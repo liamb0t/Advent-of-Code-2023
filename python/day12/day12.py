c23520bf8156ce52c34b1bc2bc6a9e3d6cb91949
Liam Mclaughlin <liammcl33@gmail.com>
2023-12-12 16:15:25 +0900

    day 12

--- python/day12/day12.py
+++ python/day12/day12.py
@@ -4,4 +4,34 @@ def read_data_from_file(file_path):
     return [line.strip() for line in data]
 
 file_path = '2023/python/day12/data.txt'
-universe = [[x for x in line] for line in read_data_from_file(file_path)]
+data = [tuple(line.split()) for line in read_data_from_file(file_path)]
+
+for seq, nums in [data[1]]:
+    combinations = []
+    queue = [int(n) for n in nums.split(',')]
+    while queue:
+        num = queue.pop(0)
+        arrangement = '.' + ''.join(['#' for _ in range(num)]) + '.'
+      
+        for i, char in enumerate(seq):
+            if char == '?':
+                possible = True
+                for s, a in zip(seq[num-i:i+num+1], arrangement):
+                    print(seq[num-i:i+num+1], num, i, queue)
+                    print(arrangement)
+                    print(s, a)
+                    print('\n') 
+                    if s == '?' and a == '.':
+                        pass
+                    if s == '.' and a == '.':
+                        pass
+                    if s == '#' and a == '#':
+                        pass
+                    if s == '.' and a == '#':
+                        possible = False
+                    if s == '#' and a == '.':
+                        possible = False
+                if possible and i not in combinations:
+                    combinations.append(i)
+
+print(combinations)
