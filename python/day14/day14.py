cbf527d52fea9003f77580d378a2e8440a4a59a6
Liam Mclaughlin <liammcl33@gmail.com>
2023-12-14 23:58:42 +0900

    part 2 refactoring

--- python/day14/day14.py
+++ python/day14/day14.py
@@ -7,32 +7,31 @@ file_path = '2023/python/day14/data.txt'
 
 platform = [list(line) for line in read_data_from_file(file_path)]
 
-def move_rocks(platform):
+def tilt(platform):
     rocks = [(i, j) for i in range(len(platform)) for j in range(len(platform[0])) if platform[i][j] == 'O']
-    total = 0
+
     for rock in rocks:
         row, col = rock[0], rock[1]
-        total += len(platform) - row
         if row > 0:
             for i, upper_row in enumerate(platform[:row][::-1], 1):
                 if upper_row[col] == '.':
-                    total += 1
                     platform[row - i][col] = 'O'
                     platform[row - i + 1][col] =  '.'  
                 else:
                     break
-    return total, platform
+    return platform
 
 def rotate(pattern):
     return [list("".join(row)) for row in zip(*pattern[::-1])]
 
+def load(platform):
+    return sum(row.count('O') * i for i, row in enumerate(platform[::-1], 1))
+
 def cycle(num_cycles, platform):
     for cycle in range(num_cycles):
         for rotation in range(4):
-            load, platform = move_rocks(platform)
-            print(load)
+            platform = tilt(platform)
             platform = rotate(platform)
-    return move_rocks(platform)
+        print(load(platform))
 
-cycle(100, platform)
-
+cycle(10000, platform)
